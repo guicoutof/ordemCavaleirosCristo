f1be1b36ee97bb91b90a2966167e6dd93f75584a
Luiz Guilherme <30446223+luiz13lg@users.noreply.github.com>
2019-10-15 16:56:47 -0300

    Inserindo a navbar

---
+++ src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+    return(
+        <div className="principal">
+            <h1>Teste</h1>
+            {/* <img src={require('../../assets/img/fundoHome.png')} className="background"/> */}
+        </div>
+    )
+}
