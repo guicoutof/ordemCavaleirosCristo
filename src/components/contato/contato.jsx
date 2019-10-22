1f372ad71d5cb5b204f4c0925e7ad0942242874f
Luiz Guilherme <30446223+luiz13lg@users.noreply.github.com>
2019-10-22 13:12:22 -0300

    iniciando

---
+++ src/components/contato/contato.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import navbar from '../navbar/navbar'
+
+export default function contato() {
+    return(
+        <div>
+            <navbar/>
+        </div>
+    ) 
+}
