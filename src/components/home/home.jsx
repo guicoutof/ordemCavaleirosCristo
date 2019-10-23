45768a3b3cc3bc6b2f76834ef52eb29fc4f351d8
Luiz Guilherme <30446223+luiz13lg@users.noreply.github.com>
2019-10-23 11:55:52 -0300

    Adicionando botão para whatsapp

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './home.css'
 
 export default () => {
-    return(
-        <div className="principal">
-            <img src={require('../../assets/img/fundoHome.png')} className="imagem"/>
-            <h1 className='titulo'>ORDEM DOS CAVALEIROS DE CRISTO</h1>
-            <h2 className='subtitulo'>Venha aprender sobre Teologia Ancestral, Cristianismo Primitivo, Kabbalah e Alquimia</h2>
-            <h2 className='subtitulo'></h2>
-        </div>
-    )
+    return <div id="home" className="principal">
+                <img src={require('../../assets/img/fundoHome.png')} alt="Background" className="imagem" />
+                <h1 className='titulo'>ORDEM DOS CAVALEIROS DE CRISTO</h1>
+                <h2 className='subtitulo'>Venha aprender sobre Teologia Ancestral, Cristianismo Primitivo, Kabbalah e Alquimia</h2>
+
+                <a href="https://wa.me/5514996903532" target='_blank'>
+                    <img src={require('../../assets/img/wpp.png')} alt="Background" className="whats" />
+                </a>
+
+            </div>
+    
 }
