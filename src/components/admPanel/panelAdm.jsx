4d7d35dca3528aab72ab7de4901e7f7fc2878c81
Luiz G Thomaz <luiz13lg13@gmail.com>
2019-12-06 15:20:35 -0300

    Painel de aulas

--- src/components/admPanel/panelAdm.jsx
+++ src/components/admPanel/panelAdm.jsx
@@ -3,6 +3,7 @@ import Navbar from '../home/navbar/navbar'
 import Footer from '../home/footer/footer'
 
 import Cursos from '../admCursos/panelCursos'
+import Aulas from './admAulas/admAulas'
 
 import './panelAdm.css'
 
@@ -16,7 +17,8 @@ export default (props)=>{
     return(
         <div className='principalADM'>
             <Navbar controle={1}></Navbar>
-            <Cursos></Cursos>
+            {/* <Cursos></Cursos> */}
+            <Aulas></Aulas>
             {/* <div className='containerADM'>
                 {
                 modulos.map((modulo) =>
