23887c34534afceb500948c336ce580a800e4a24
Guilherme Couto <guilherme.couto@unesp.br>
2019-12-19 16:01:11 -0300

    Hotfix Curso
    Visualização e alterações do background do curso, link para visualização do componente & rota de serviço utilizando componente dos cursos

---
+++ src/components/userPanel/curso/curso.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import MdArrowBack from 'react-ionicons/lib/MdArrowBack';
+import './curso.css';
+
+export class Curso extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            moduleTitle: "",
+            moduleID: 0,
+            iframeLink: "",
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="curso-box">
+                <div className="curso-container">
+                    <div className="curso-button-content">
+                        <div className="curso-module">
+                            <div className="curso-image-header"></div>
+                            <h2 className="curso-h2">Estudo Judaico</h2>
+                            <p className="curso-module-name">Módulo 1</p>
+                        </div>
+                        <div className="curso-module-btn-group">
+                            <button className="curso-module-btn">Aula 1</button>
+                            <button className="curso-module-btn">Aula 2</button>
+                            <button className="curso-module-btn">Aula 3</button>
+                            <button className="curso-module-btn">Aula 4</button>
+                            <button className="curso-module-btn">Aula 5</button>
+                            <button className="curso-module-btn">Aula 6</button>
+                            <button className="curso-module-span"><MdArrowBack /> Módulos</button>
+                        </div>
+                    </div>
+                    <div className="curso-content">
+                        <header className="curso-header-name">
+                            <h1 className="curso-title">
+                                Estudo Judaico
+                                </h1>
+                        </header>
+                        <iframe title="cursoFrame" className="curso-video" src="https://www.youtube.com/embed/w-IXajuNAP8" frameBorder="0" allowFullScreen></iframe>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Curso;
