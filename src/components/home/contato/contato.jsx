b568228a0ba727b0c00af04a0cbb4a7fb79df899
Guilherme Couto <guilherme.couto@unesp.br>
2020-01-07 13:05:14 -0300

    Posts
    Post de envio de cadastro do usuário e de contato por email

--- src/components/home/contato/contato.jsx
+++ src/components/home/contato/contato.jsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
+import api from "../../../services/api";
 import './contato.css'
 
-export default function contato() {
-    return(
-        <div className="backimg">
+export default class Contato extends Component {
+    state = {
+        email:"",
+        name:"",
+        subject:"",
+        message:"",
+        error:""
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        console.log(this.state);
+        const{email,name,subject,message} = this.state;
+        if(!email || !name ||  !subject || !message ){
+          this.setState({error: "Preencha todos os campos para continuar!"})
+        }else{
+            try{
+                console.log(this.state);
+                const response = await api.post("/contact",{email,name,subject,message});
+                console.log(response);
+
+                this.setState({error:"Email enviado com sucesso",name:"",email:"",subject:"",message:""});
+            }catch(err){
+                console.log(err)
+            }
+            
+        }
+        
+      }
+
+    render(){
+
+        return(
+            <div className="backimg">
             <div className='contato'>
                 <h1 className='contatoTitulo'>CONTATO</h1>
             </div>
             <form action="" className='formulario'>
-                <input type="text" placeholder='Nome' className='inputSimples'/>
-                <input type="text" placeholder='Email' className='inputSimples'/>
-                <input type="text" placeholder='Assunto' className='inputSimples'/>
-                <textarea placeholder='Mensagem' className='inputTexto'></textarea>
+                {this.state.error}
+                <input type="text" placeholder='Nome' className='inputSimples' value={this.state.name} onChange={e=>this.setState({name:e.target.value})}/>
+                <input type="email" placeholder='Email' className='inputSimples' value={this.state.email} onChange={e=>this.setState({name:e.target.email})}/>
+                <input type="text" placeholder='Assunto' className='inputSimples' value={this.state.assunto} onChange={e=>this.setState({name:e.target.subject})}/>
+                <textarea placeholder='Mensagem' className='inputTexto' value={this.state.message} onChange={e=>this.setState({name:e.target.message})}></textarea>
 
-                <input type="submit" className='submit'/>
+                <input type="submit" className='submit' onClick={this.handleSubmit}/>
             </form>            
         </div>
     ) 
+    }
 }
