23528ae928d2c783253e74247a5a5011ae2d8302
Luiz G Thomaz <luiz13lg13@gmail.com>
2020-01-16 12:03:53 -0300

    manutenção css cursos administrador

--- src/components/admPanel/cursos/panelCursos.jsx
+++ src/components/admPanel/cursos/panelCursos.jsx
@@ -13,7 +13,6 @@ export default class panelCurso extends Component{
             modalC:false,
             id:0
         }
-        
         this.close = this.close.bind(this)
         this.confirm = this.confirm.bind(this)
     }
@@ -55,21 +54,21 @@ export default class panelCurso extends Component{
                 <div className="tabelaCursos">
                     {
                         this.state.courses.map(course=>
-                            <div key={course.id} className="divCursos">
+                            <div key={course.id} className="divListaCursos">
                                 <div className="infoCurso">
                                     <img src={course.url} alt={course.path}className="imgCurso"/>
                                     <div className="infoTexto">
                                         <h5 className="nomeCurso">{course.name}</h5>
                                         {/* <p className="descricaoCurso">{course.id}</p> */}
                                         {/* <p className="descricaoCurso">{course.module_id}</p> */}
-                                        <p className="descricaoCurso">{course.description}</p>
-                                        <p className="descricaoCurso">{course.hours}</p>
-                                        <p className="descricaoCurso">{course.assistance}</p>
-                                        <p className="descricaoCurso">{course.book}</p>
-                                        <p className="descricaoCurso">{course.highlight?'Destaque':'Sem destaque'}</p>
+                                        <p className="descricaoListaCurso"><b>Descrição do Curso:</b> {course.description}</p>
+                                        <p className="descricaoListaCurso"><b>Horas:</b> {course.hours}</p>
+                                        <p className="descricaoListaCurso"><b>Assistência:</b> {course.assistance}</p>
+                                        <p className="descricaoListaCurso"><b>Livro:</b> {course.book}</p>
+                                        <p className="descricaoListaCurso"><b>Destaque:</b> {course.highlight?'Destaque':'Sem destaque'}</p>
                                     </div>
                                 </div>
-                                <h4 className="precoCurso">{course.price}</h4>
+                                <h4 className="precoCurso">Valor: {course.price}</h4>
                                 <div className="botoesCurso">
                                     <NavLink to={`/course/${course.id}`}><button className="botaoEditarCurso">Abrir</button></NavLink>
                                     <NavLink to={`/course/${course.id}/edit`}><button className="botaoEditarCurso">Editar</button></NavLink>
@@ -78,7 +77,7 @@ export default class panelCurso extends Component{
                             </div>
                         )    
                     }
-                    <NavLink to={`/modules`}><button>Voltar</button></NavLink>
+                    <NavLink to={`/modules`}><button className="botaoVoltar">Voltar</button></NavLink>
                 </div>
                 
             </div>
