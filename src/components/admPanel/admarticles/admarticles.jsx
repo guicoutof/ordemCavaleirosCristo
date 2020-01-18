175ad02466436a2142e918d3e3a47f0b73b9d8c8
Luiz G Thomaz <luiz13lg13@gmail.com>
2020-01-17 23:25:38 -0300

    manutenção blog admin
    botões avançar e voltar página

--- src/components/admPanel/admarticles/admarticles.jsx
+++ src/components/admPanel/admarticles/admarticles.jsx
@@ -74,18 +74,10 @@ export default class PanelBlog extends Component {
                             )
                         }
                     </div>
-                    <div>
-                        {this.state.page>1?<button onClick={()=>this.exibirCursos(this.state.page-1)}>Pagina Anterior</button>:<div></div>}
-                        {!this.state.limite?<button onClick={()=>this.exibirCursos(this.state.page+1)}>Proxima Pagina</button>:<div></div>}
-                    </div>
                     <div className="containerbuttons2">
-                        <button className="getback">
-                            <p> VOLTAR </p>
-                        </button>
+                        {this.state.page>1?<button className='botaoVoltar' onClick={()=>this.exibirCursos(this.state.page-1)}>Pagina Anterior</button>:<div></div>}
+                        {!this.state.limite?<button className='botaoVoltar' onClick={()=>this.exibirCursos(this.state.page+1)}>Proxima Pagina</button>:<div></div>}
                     </div>
-
-
-
                 </div>
             </div>
 
