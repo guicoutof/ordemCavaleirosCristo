b0ee51bf1ff28c8f89d551fb5f4b6d05f9efb7f8
Guilherme Couto <guilherme.couto@unesp.br>
2020-01-19 06:47:16 -0300

    Comentarios

--- src/components/home/reviews/userReviews.jsx
+++ src/components/home/reviews/userReviews.jsx
@@ -12,58 +12,51 @@ export default class Feedbacks extends Component{
         this.state={
             carrosel:0,
             comments:[],
+            comment:[]
         }
     }
-    // const [carousel, setCarousel] = useState(0);
-    // let cards = [];
+
     async componentDidMount(){
         const response = await api.get('/approvedComments')
-        console.log(response.data)
         this.setState({comments:response.data})
+        this.exibirComentario(0)
     }
 
+    exibirComentario(n){
+        this.setState({carrosel:n})
+        this.setState(state=>{
+            const comment = state.comments.filter((c,i)=>{
+                if(i===n)return c;
+                else return null;
+            })
+            return {comment}
+        })
+    }
+    
     render(){
-let cards = []
         return (
             <div className="feedback">
             <div className="title">
                 <h1>VEJA O QUE NOSSOS ESTUDANTES ACHARAM</h1>
             </div>
             <div className="carousel">
-                <li className="pointer" onClick={()=> this.setState(state=>{
-                    let carrosel=0;
-                    if((state.carrosel-1)>=0){
-                        carrosel=(state.carrosel-1);
-                        return{carrosel}
-                    }else return {carrosel}
-                })}><FontAwesomeIcon icon={faCaretLeft} size="3x"/></li>
-                {
-                    cards = this.state.comments.filter((c,i)=>{
-                        if(i===this.state.carrosel)return c;
-                        else return null;
-                    })
-                    ,
-                        cards.map((c)=>
-                        <div className="cardCarousel" key={c.id}>
-                                <h3 className="name">{c.name}</h3>
-                                <h4 className="text">{c.text}</h4>
-                            </div>
-                        )
-                    } 
-                <li className="pointer" onClick={()=> this.setState(state=>{
-                    let carrosel=state.carrosel
-                    if((state.carrosel+1)<state.comments.length){
-                        carrosel=carrosel+1;
-                        return {carrosel}
-                    }else return {carrosel}
-                }) }><FontAwesomeIcon icon={faCaretRight} size="3x"/></li>
+                <li className="pointer" onClick={()=> ((this.state.carrosel-1)>=0)? this.exibirComentario(this.state.carrosel-1) : this.exibirComentario(0) }><FontAwesomeIcon icon={faCaretLeft} size="3x"/></li>
+                {this.state.comment.map(c=>
+                    <div className="cardCarousel" key={c.id}>
+                            {c.status===2?<h3 className="name">{c.user.name}</h3>:<h3 className="name">Usuário Anônimo</h3>}
+                            <h4 className="text">{c.content}</h4>
+                        </div>
+                    )
+                }
+                
+                <li className="pointer" onClick={()=> ((this.state.carrosel+1)<this.state.comments.length)? this.exibirComentario(this.state.carrosel+1) : this.exibirComentario(this.state.carrosel) }><FontAwesomeIcon icon={faCaretRight} size="3x"/></li>
             </div>
             <div className="list">{
                 this.state.comments.map((c,i)=>
                 i=== this.state.carrosel?
-                <li key={i} id="visitedCircle">{<FontAwesomeIcon className="iconCircle" icon={faCircle} onClick={()=> this.setState({carrosel:i})} />}</li>
+                <li key={i} id="visitedCircle">{<FontAwesomeIcon className="iconCircle" icon={faCircle} onClick={()=> this.exibirComentario(i)} />}</li>
                 : 
-                <li key={i}>{<FontAwesomeIcon className="iconCircle" icon={faCircle} onClick={()=> this.setState({carrosel:i})} />}</li>
+                <li key={i}>{<FontAwesomeIcon className="iconCircle" icon={faCircle} onClick={()=> this.exibirComentario(i)} />}</li>
                 )
             }</div>
         </div>
