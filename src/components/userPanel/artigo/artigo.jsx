cb43fb1b82224c121a32cd56aeea967ffef66f39
guicouto <guicoutof@hotmail.com>
2020-02-17 15:12:07 -0300

    Alterações Cliente
    Mosaico Blog

--- src/components/userPanel/artigo/artigo.jsx
+++ src/components/userPanel/artigo/artigo.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './artigo.css'
+import Modal from 'react-modal';
 
-export default function blog() {
+export default function artigo(props) {
+    const title = props.title;
+    const url = props.url;
+    const text = props.text;
     return(
-        <div className="containerarticles">
-            <div className="containerarticles">
-                <div className="divPost">
-                    <h1 className="artname0">No princípio D'us crious os Céus e a Terra</h1>
-                </div>
-                <div className="thetext">
-                    <p className="divTexto">[...] A teoria aceita hoje pela maioria dos cientistas é a teoria do Big Bang, apresentada em 1946 por George Gamow e que pode ser assim expressa: num dado instante, nosso Universo não existia, e no instante seguinte, passou a existir. Segundo a teoria, há 15 bilhões de anos, apareceu subitamente do nada uma colossal fonte de energia, chamada de “bola de fogo primordial”. Sabemos também, através da teoria da relatividade do físico Albert Einstein, que o espaço é curvo e se comporta como o movimento das águas, devido a força da gravidade que pressiona a matéria para baixo, de acordo com a sua massa; quanto maior a massa, menor será sua volatilidade, e vice-versa, e maior será o seu campo gravitacional e e vice-versa. Portanto, o Espírito de D’us pairava sobre a matéria escura, criou os céus (o espaço) e a terra (apesar de conter massa - matéria - é sem forma e vazia. A mesma classificação que um astrofísico faria para um buraco negro) [...]”</p>
-                </div>
+        <Modal className="modalArtigo" isOpen={props.open} onRequestClose={props.handleClose} ariaHideApp={false} >
+            <div className="divArtigo">
+                <h1>{title}</h1>       
+                <img className="art0" src={url} alt={title} />
+                <div className="divTexto" dangerouslySetInnerHTML={{__html: text}}></div>
             </div>
-               
-        </div>
+        </Modal>
     ) 
 }
--- src/components/userPanel/blog/blog.jsx
+++ src/components/userPanel/blog/blog.jsx
@@ -3,6 +3,8 @@ import './blog.css'
 import api from '../../../services/api'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCircleNotch} from '@fortawesome/free-solid-svg-icons';
+import Artigo from '../artigo/artigo'
+
 
 
 
@@ -13,8 +15,16 @@ export default class Blog extends Component{
             posts:[],
             pages:1,
             limite:false,
-            loading:true
+            loading:true,
+            modal:false,
+            article:{
+                title:'',
+                url:'',
+                text:''
+            }
         }
+
+        this.handleClose.bind(this);
     }
 
     async exibirCursos(id){
@@ -33,22 +43,38 @@ export default class Blog extends Component{
         this.exibirCursos(1)
     }
 
+    handleOpen(article){
+        this.setState({modal:true,article:article})
+    }
+
+    handleClose(){
+        this.setState({modal:false})
+    }
+
     render(){
         return(
-        <div className="container1">
-            {this.state.loading?<FontAwesomeIcon className="icon" icon={faCircleNotch} size="3x" spin/>
-                :this.state.posts.length>0?
-                this.state.posts.map(post=>
-                <div className="divPost" key={post.id}>
-                    <h1 className="artname0">{post.title}</h1>       
-                    <img className="art0" src={post.url} alt={post.title} />
-                    <div className="divTexto" dangerouslySetInnerHTML={{__html: post.text}}></div>
+        <div className="backimg">
+            <div className='contato'>
+                <h1 className='contatoTitulo'>BLOG</h1>
+            </div>
+            <div className="containerbloguser">
+                <Artigo open={this.state.modal} handleClose={()=>this.handleClose()}
+                    title={this.state.article.title} url={this.state.article.url} text={this.state.article.text}
+                    />
+                {this.state.loading?<FontAwesomeIcon className="icon" icon={faCircleNotch} size="3x" spin/>
+                    :this.state.posts.length>0?
+                    this.state.posts.map(post=>
+                        <div className="divPostUser" key={post.id} onClick={()=>this.handleOpen(post)}>
+                        {/* <h1 className="artname0">{post.title}</h1>        */}
+                        <img className="imgPost" src={post.url} alt={post.title} />
+                        {/* <div className="divTexto" dangerouslySetInnerHTML={{__html: post.text}}></div> */}
+                    </div>
+                    ):<h3 className="divPost">Ainda não há nenhum artigo</h3>
+                }
+                <div>
+                    {this.state.page>1?<button className="botaoVoltar" onClick={()=>this.exibirCursos(this.state.page-1)}>Pagina Anterior</button>:<div></div>}
+                    {!this.state.limite?<button className="botaoVoltar" onClick={()=>this.exibirCursos(this.state.page+1)}>Proxima Pagina</button>:<div></div>}
                 </div>
-                ):<h3 className="divPost">Ainda não há nenhum artigo</h3>
-            }
-            <div>
-                {this.state.page>1?<button className="botaoVoltar" onClick={()=>this.exibirCursos(this.state.page-1)}>Pagina Anterior</button>:<div></div>}
-                {!this.state.limite?<button className="botaoVoltar" onClick={()=>this.exibirCursos(this.state.page+1)}>Proxima Pagina</button>:<div></div>}
             </div>
         </div>
         )
