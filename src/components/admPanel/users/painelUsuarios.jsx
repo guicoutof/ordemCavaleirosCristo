86fff44cf842aff6ffdfdecb213d4f36c083ff3d
guicouto <guicoutof@hotmail.com>
2020-05-12 14:20:22 -0300

    Hotfix Exclusão de usuário

--- src/components/admPanel/users/painelUsuarios.jsx
+++ src/components/admPanel/users/painelUsuarios.jsx
@@ -87,6 +87,7 @@ export default class panelUsuarios extends Component {
     }
 
     removeUser(i){
+        console.log(i)
         this.setState({modalC:true,id:i})
     }
 
@@ -100,136 +101,6 @@ export default class panelUsuarios extends Component {
         await api.delete(`/deleteStudent/${id}`)
         window.location.reload();
     }
-
-    // showCreateUser() {
-    //     return(
-    //         <div className="panelCreateAndEditUser-container">
-    //             <div className="panelCreateAndEditUser-form-header-div">
-    //                 <h2 className="panelCreateAndEditUser-form-header">
-    //                     Usuário
-    //                 </h2>
-    //             </div>
-    //             <form className="panelCreateAndEditUser-form">
-    //                 <div className="panelCreateAndEditUser-user-input-group">
-    //                     <input className="panelCreateAndEditUser-inputs" placeholder="Nome do usuário" onChange={this.handleCreateUser}/>
-    //                     <input className="panelCreateAndEditUser-inputs" placeholder="Email" onChange={this.handleCreateUser}/>
-    //                     <input className="panelCreateAndEditUser-inputs" placeholder="Telefone/Celular" onChange={this.handleCreateUser}/>
-    //                 </div>
-    //                 <div className="panelCreateAndEditUser-user-input-group">
-    //                     <input className="panelCreateAndEditUser-inputs" placeholder="Cidade" onChange={this.handleCreateUser}/>
-    //                     <input className="panelCreateAndEditUser-inputs" placeholder="Estado" onChange={this.handleCreateUser}/>
-    //                     <input className="panelCreateAndEditUser-inputs" placeholder="País" onChange={this.handleCreateUser}/>
-    //                 </div>
-    //                 <div className="panelCreateAndEditUser-footer-group">
-    //                     <h1>Tipo de Cadastro</h1>
-    //                     <ul className="panelCreateAndEditUser-ul">
-    //                         <li>
-    //                             <span className="panelCreateAndEditUser-free">
-    //                                 FREE - 0
-    //                             </span>
-    //                             <span className="panelCreateAndEditUser-premium">
-    //                                 PENDENTE - 1
-    //                             </span>
-    //                             <span className="panelCreateAndEditUser-premium">
-    //                                 AFILIADO - 2
-    //                             </span>
-    //                         </li>
-    //                         <li>
-    //                             <input className="panelCreateAndEditUser-checkboxe" type="radio" name="accountType" onChange={this.handleCreateUser}/>
-    //                             <input className="panelCreateAndEditUser-checkboxe" type="radio" name="accountType" onChange={this.handleCreateUser}/>
-    //                             <input className="panelCreateAndEditUser-checkboxe" type="radio" name="accountType" onChange={this.handleCreateUser}/>
-    //                         </li>
-    //                     </ul>
-    //                 </div>
-    //             </form>
-    //             <button className="panelCreateAndEditUser-submit-btn" onClick={console.log("Não sei o q fazer aqui")}>SALVAR</button>
-    //             <div className="panelCreateAndEditUser-back-btn-div">
-    //                 <button className="panelCreateAndEditUser-back-btn" onClick={this.openUserList.bind(this)}><MDArrowBack /> VOLTAR</button>
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
-    // showEditUser() {
-    //     return(
-    //         <div className="panelCreateAndEditUser-container">
-    //             <div className="panelCreateAndEditUser-form-header-div">
-    //                 <h2 className="panelCreateAndEditUser-form-header">
-    //                     Edição de Usuário
-    //                     {this.state.msg}
-    //                 </h2>
-    //             </div>
-
-    //             <div className="panelCreateAndEditUser-form">
-    //                 <div className="panelCreateAndEditUser-user-input-group">
-    //                     <div className="inputs">
-    //                         <label htmlFor="nome">Nome</label>
-    //                         <input id="nome" className="panelCreateAndEditUser-inputs" placeholder="Nome do usuário"  value={this.state.name||''} onChange={e=>this.setState({name:e.target.value})}/>
-    //                     </div>
-                        
-    //                     <div className="inputs">
-    //                         <label htmlFor="email">Email</label>
-    //                         <input id="email" className="panelCreateAndEditUser-inputs" placeholder="Email" value={this.state.email||''} onChange={e=>this.setState({email:e.target.value})}/>
-    //                     </div>
-                        
-    //                     <div className="inputs">
-    //                         <label htmlFor="tel">Telefone</label>
-    //                         <input id="tel" className="panelCreateAndEditUser-inputs" placeholder="Telefone/Celular" value={this.state.phone_number||''} onChange={e=>this.setState({phone_number:e.target.value})}/>
-    //                     </div>
-                        
-    //                     <div className="inputs">
-    //                         <label htmlFor="data">Data de Nascimento</label>
-    //                         <input id="data" type="date" className="panelCreateAndEditUser-inputs" placeholder="Data de Nascimento" value={this.state.birth_date||''} onChange={e=>this.setState({birth_date:e.target.value})}/>
-    //                     </div>
-    //                 </div>
-                    
-    //                 <div className="panelCreateAndEditUser-user-input-group">
-    //                     <div className="inputs">
-    //                         <label htmlFor="cidade">Cidade</label>
-    //                         <input id="cidade" className="panelCreateAndEditUser-inputs" placeholder="Cidade" value={this.state.city||''} onChange={e=>this.setState({city:e.target.value})}/>
-    //                     </div>
-
-    //                     <div className="inputs">
-    //                         <label htmlFor="estado">Estado</label>
-    //                         <input id="estado" className="panelCreateAndEditUser-inputs" placeholder="Estado" value={this.state.state||''} onChange={e=>this.setState({state:e.target.value})}/>
-    //                     </div>
-
-    //                     <div className="inputs">
-    //                         <label htmlFor="pais">País</label>
-    //                         <input id="pais" className="panelCreateAndEditUser-inputs" placeholder="País" value={this.state.country||''} onChange={e=>this.setState({country:e.target.value})}/>
-    //                     </div>
-
-    //                     <div className="inputs">
-    //                         <label htmlFor="modAtual">Modulo atual</label>
-    //                         <input id="modAtual" type="number" className="panelCreateAndEditUser-inputs" placeholder="Modulo" value={this.state.module||1} onChange={e=>this.setState({module:e.target.value})}/>
-    //                     </div>
-    //                 </div>
-
-    //                 <div className="panelCreateAndEditUser-footer-group">
-    //                     <h1>Tipo de Cadastro</h1>
-    //                     <div className="divCheckbox">
-    //                         <div className="inputsCheckbox">
-    //                             <label htmlFor="free">Free</label>
-    //                             <input id="free" className="panelCreateAndEditUser-checkboxe" type="radio" name="accountType" value={this.state.type} checked={!this.state.type} onChange={e=>this.setState({type:0})}/>
-    //                         </div>
-    //                         <div className="inputsCheckbox">
-    //                             <label htmlFor="pendente">Pendente</label>
-    //                             <input id="pendente" className="panelCreateAndEditUser-checkboxe" type="radio" name="accountType" value={this.state.type} checked={this.state.type==1?true:false} onChange={e=>this.setState({type:1})}/>
-    //                         </div>
-    //                         <div className="inputsCheckbox">
-    //                             <label htmlFor="afiliado">Afiliado</label>
-    //                             <input id="afiliado" className="panelCreateAndEditUser-checkboxe" type="radio" name="accountType" value={this.state.type} checked={this.state.type==2?true:false} onChange={e=>this.setState({type:2})}/>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             <button className="panelCreateAndEditUser-submit-btn" onClick={()=>this.submitUser()}>SALVAR</button>
-    //             <div className="panelCreateAndEditUser-back-btn-div">
-    //                 <button className="panelCreateAndEditUser-back-btn" onClick={this.openUserList.bind(this)}><MDArrowBack /> VOLTAR</button>
-    //             </div>
-    //         </div>
-    //     );
-    // }
 
     async approveUser(id,type){
         await api.post(`/approveStudent/${id}`,{type})
@@ -319,7 +190,7 @@ export default class panelUsuarios extends Component {
                                     </abbr>
                                 </button>:<div></div>}
                                 {/* <NavLink to={`/user/${element.id}/course`}><button>Cursos</button></NavLink> */}
-                                <button className="panelUser-btn btn-excluir" onClick={()=>this.removeUser(i)}>
+                                <button className="panelUser-btn btn-excluir" onClick={()=>this.removeUser(element.id)}>
                                     <FontAwesomeIcon className="icon" icon={faTrash} size="3x"/>
                                 </button>
                                 <Confirm open={this.state.modalC}  title={'Deseja realmente excluir este usuario?'} close={this.close} confirm={this.confirm}/> 
